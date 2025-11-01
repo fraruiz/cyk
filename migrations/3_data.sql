6c635020517164e2b491d7dd634a94cad7c9194b
fraruiz <franciscoruizlezcano@gmail.com>
2025-10-31 21:26:32 -0300

    add data insert for glc

---
+++ migrations/3_data.sql
@@ -0,0 +1,10 @@
+\set content `cat /docker-entrypoint-initdb.d/glc.json`
+
+INSERT INTO GLC (START, LEFT_SYMBOL, FIRST_RIGHT_SYMBOL, SECOND_RIGHT_SYMBOL, TYPE)
+SELECT
+    (elem->>'START')::boolean,
+    elem->>'LEFT_SYMBOL',
+    elem->>'FIRST_RIGHT_SYMBOL',
+    elem->>'SECOND_RIGHT_SYMBOL',
+    (elem->>'TYPE')::smallint
+FROM jsonb_array_elements(:'content'::jsonb) AS elem;
